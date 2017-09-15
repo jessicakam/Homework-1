e3996f7ab9fc79e6d9cd30025c9ab864293be721
Jessica Kam <jessica.c.kam@gmail.com>
2017-09-15 00:19:36 -0700

    completed fib

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -3,8 +3,21 @@ pragma solidity ^0.4.16;
 
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		if (position == 0) {
+			return 0;
+		} else {
+			uint prev = 0;
+			uint curr = 1;
+			uint i;
+			for (i = 1; i < position; i++) {
+				uint temp = prev + curr;
+				prev = curr;
+				curr = temp;
+			}
+			return result;
+		}
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() payable { }
 }
